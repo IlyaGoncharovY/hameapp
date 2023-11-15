15a6448ad156e32379ddbba3b93a65e66d0c5a26
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-11-15 20:39:41 +0300

    fix(StocksContainer) render stock and use hook RTKQ

--- src/features/stocks/components/StocksContainer.tsx
+++ src/features/stocks/components/StocksContainer.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {FC} from 'react';
 
-export const StocksContainer = () => {
+import {homeAPI} from "../../../api/HomeAppService";
+import {StockUserItem} from "../../users/components/StockUserItem";
+import {HousesResponseTypeChild, StreetsResponseTypeChild} from "../../../common/interfaces/Interfaces";
+
+interface IStocksContainer {
+    house: HousesResponseTypeChild
+    street: StreetsResponseTypeChild
+}
+
+export const StocksContainer: FC<IStocksContainer> = ({street, house}) => {
+
+    const {
+        data: stocks,
+        error,
+        isLoading
+    } = homeAPI.useGetStocksQuery({streetsId: street.id, housesId: house.id})
+
     return (
-        <div>
-            StocksContainer
+        <div style={{border: "2px solid gold"}}>
+            <div>StocksContainer</div>
+            <div>
+                <div>Номер дома: {house.id}</div>
+                <div>Имя дома: {house.name}</div>
+            </div>
+            <div>
+                {stocks && stocks.map(stock =>
+                    <StockUserItem
+                        key={stock.addressId}
+                        stock={stock}
+                    />)}
+            </div>
         </div>
     );
 };
