d97c2a05332a296359e373c3724cbf289420a4ef
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-11-15 20:17:36 +0300

    fix(StreetsContainer) added render streets use hook RTKQ

--- src/features/streets/components/StreetsContainer.tsx
+++ src/features/streets/components/StreetsContainer.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+
+import {homeAPI} from "../../../api/HomeAppService";
 import {HomesContainer} from "../../homes/components/HomesContainer";
 
 export const StreetsContainer = () => {
+
+    const {data: streets, error, isLoading} = homeAPI.useGetAllStreetsQuery('')
+
     return (
-        <div>
-            StreetsContainer
-            <HomesContainer/>
+        <div style={{border: "2px solid black"}}>
+            <div>StreetsContainer</div>
+            <div>
+                {streets && streets.map((street) =>
+                    <HomesContainer key={street.id}
+                                    street={street}
+                    />)}
+            </div>
         </div>
     );
 };
