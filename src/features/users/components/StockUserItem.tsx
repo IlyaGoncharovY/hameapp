a856eb575b6592730a34ed7e6856595c5d84574c
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-11-16 22:02:35 +0300

    fix(StockUserItem) added logic for render error and loaded, render users, added style

--- src/features/users/components/StockUserItem.tsx
+++ src/features/users/components/StockUserItem.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 
 import {ItemUser} from "./item/ItemUser";
+import {homeAPI} from "../../../api/HomeAppService";
 import {StocksResponseTypeChild} from "../../../common/interfaces/Interfaces";
 
 interface IStockUserItem {
@@ -8,13 +9,27 @@ interface IStockUserItem {
 }
 
 export const StockUserItem: FC<IStockUserItem> = ({stock}) => {
+    const {
+        data: users,
+        error,
+        isLoading
+    } = homeAPI.useGetUsersQuery(stock.addressId)
+
+    if (isLoading) {
+        return <h1>...Загрузка жильца</h1>
+    }
+
+    if (error) {
+        return <h1>Произошла ошибка</h1>
+    }
+
     return (
-        <div style={{border: "2px solid green"}}>
-            {/*<div>StockUserItem</div>*/}
-            {stock.accounts.map(userInfo =>
+        <div style={{padding: "20px", margin: "20px"}}>
+            {users && users.map(user =>
                 <ItemUser
-                    key={stock.flat}
-                    userInfo={userInfo}
+                    key={user.id}
+                    user={user}
+                    isLoadingUsers={isLoading}
                 />)}
         </div>
     );
