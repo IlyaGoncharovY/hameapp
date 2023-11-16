12121c95d57e07c1338a4f971209473a57729d95
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-11-16 21:56:41 +0300

    fix(types) added types for add and delete user

--- src/common/interfaces/Interfaces.ts
+++ src/common/interfaces/Interfaces.ts
@@ -1,7 +1,7 @@
 /**
  * streets common type
- * @param StreetsResponseTypeChildPrefix
- * @param StreetsResponseTypeChild
+ * @type StreetsResponseTypeChildPrefix
+ * @type StreetsResponseTypeChild
  */
 export type StreetsResponseType = StreetsResponseTypeChild[];
 export type StreetsResponseTypeChildPrefix = {
@@ -20,7 +20,7 @@ export type StreetsResponseTypeChild = {
 
 /**
  * hoses common type
- * @param HousesResponseTypeChild
+ * @type HousesResponseTypeChild
  */
 export type HousesResponseType = HousesResponseTypeChild[];
 export type HousesResponseTypeChild = {
@@ -30,9 +30,9 @@ export type HousesResponseTypeChild = {
 
 /**
  * stocks common type
- * @param StocksResponseTypeChild
- * @param StocksResponseTypeChildAccounts
- * @param StocksResponseTypeChildAccountsType
+ * @type StocksResponseTypeChild use addressId: number
+ * @type StocksResponseTypeChildAccounts
+ * @type StocksResponseTypeChildAccountsType
  */
 export type StocksResponseType = StocksResponseTypeChild[];
 export type StocksResponseTypeChildAccountsType = {
@@ -54,3 +54,44 @@ export type StocksResponseTypeChild = {
 	building: string;
 	flat: string;
 }
+/**
+ * type addUsers response
+ * @type addUserResponseType res
+ * @type id (number)
+ * @type name (string)
+ * @type phone (string)
+ * @type email (string)
+ * @type bindId (number)
+ */
+export type addUserResponseType = {
+	id: number;
+	name: string;
+	phone: string;
+	email: string;
+	bindId: number
+}
+
+/**
+ * type res add user
+ * @type addUserRequestType request
+ * @type id userId(number)
+ * @type result
+ */
+export type addUserResponseSuccessType = {
+	id: number;
+	result: string;
+}
+
+/**
+ * type req add user
+ * @type addUserResponseType res
+ * @type name (string)
+ * @type phone (string
+ * @type email (string)
+ */
+export type addUserRequestType = {
+	name: string;
+	phone: string;
+	email: string;
+}
+
