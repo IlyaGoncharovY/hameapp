1121b3e97437a41f226f4a44359ab3e1eefe7139
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-11-16 21:57:49 +0300

    fix(api) added endpoints for get, pos, put, delete users in apartments

--- src/api/HomeAppService.ts
+++ src/api/HomeAppService.ts
@@ -1,29 +1,71 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/dist/query/react';
 
-import {HousesResponseType, StocksResponseType, StreetsResponseType} from '../common/interfaces/Interfaces';
+import {
+    addUserRequestType, addUserResponseSuccessType,
+    addUserResponseType,
+    HousesResponseType,
+    StocksResponseType,
+    StreetsResponseType
+} from '../common/interfaces/Interfaces';
 
 export const homeAPI = createApi({
     reducerPath: 'homeAPI',
     baseQuery: fetchBaseQuery({baseUrl: 'https://dispex.org/api/vtest/'}),
+    tagTypes: ["PUT"],
     endpoints: builder => ({
         getAllStreets: builder.query<StreetsResponseType, string>({
             query: () => ({
-                url: '/Request/streets',
+                url: 'Request/streets',
             }),
         }),
         getCurrentHouses: builder.query<HousesResponseType, number>({
             query: (streetId: number) => ({
-                url: `/Request/houses/${streetId}`,
+                url: `Request/houses/${streetId}`,
             }),
         }),
         getStocks: builder.query<StocksResponseType, { streetsId: number, housesId: number }>({
-            query: ({ streetsId, housesId }) => ({
-                url: `/HousingStock`,
+            query: ({streetsId, housesId}) => ({
+                url: `HousingStock`,
                 params: {
                     streetId: streetsId,
                     houseId: housesId
                 }
             }),
+        }),
+        getUsers: builder.query<addUserResponseType[], number>({
+            query: (addressId: number) => ({
+                url: `HousingStock/clients`,
+                params: {
+                    addressId: addressId
+                }
+            }),
+            providesTags: result => ["PUT"]
+        }),
+        addNewUser: builder.mutation<addUserResponseSuccessType, addUserRequestType>({
+            query: (newUser) => ({
+                url: 'HousingStock/client',
+                method: "POST",
+                body: newUser
+            }),
+
+        }),
+        bindUserInApartment: builder.mutation<void, { addressId: number, clientId: number }>({
+            query: ({addressId, clientId}) => ({
+                url: `HousingStock/bind_client`,
+                method: "PUT",
+                body: {
+                    addressId: addressId,
+                    clientId: clientId
+                },
+            }),
+            invalidatesTags: ["PUT"],
+        }),
+        deleteUser: builder.mutation<void, number>({
+            query: (bindId) => ({
+                url: `HousingStock/bind_client/${bindId}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["PUT"],
         }),
     }),
 });
