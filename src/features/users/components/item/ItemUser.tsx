59a090aae437ff0ea6f69cbce46dd09f434c3c15
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-11-16 22:03:37 +0300

    fix(ItemUser) added logic for render error and loaded, render users, added style

--- src/features/users/components/item/ItemUser.tsx
+++ src/features/users/components/item/ItemUser.tsx
@@ -1,21 +1,41 @@
 import React, {FC} from 'react';
 
-import {StocksResponseTypeChildAccounts} from "../../../../common/interfaces/Interfaces";
+import {homeAPI} from "../../../../api/HomeAppService";
+import {addUserResponseType} from "../../../../common/interfaces/Interfaces";
 
 interface IItemUser {
-    userInfo: StocksResponseTypeChildAccounts
+    user: addUserResponseType
+    isLoadingUsers: boolean
 }
 
-export const ItemUser: FC<IItemUser> = ({userInfo}) => {
+export const ItemUser: FC<IItemUser> = ({user, isLoadingUsers}) => {
+
+    const [deleteUser] = homeAPI.useDeleteUserMutation()
+
+    const deleteUserHandler = (bindId: number) => {
+        deleteUser(bindId)
+            .unwrap()
+            .then(() => {
+                console.log("User deleted successfully");
+            })
+            .catch((error) => {
+                console.error("Error deleting user:", error);
+            });
+    }
+
     return (
-        <div>
+        <div style={{padding: "20px", border: "2px solid purple", margin: "20px"}}>
             <div>
-                ItemUser
+                Жилец
             </div>
             <div>
-                <div>Имя аккаунта: {userInfo.account}</div>
-                <div>ID: {userInfo.type.id}</div>
-                <div>Имя: {userInfo.type.name}</div>
+                <div>Имя жильца: {user.name}</div>
+                <div>Телефон жильца: {user.phone}</div>
+                <div>Email жильца: {user.email}</div>
+                <div>ID: {user.id}</div>
+            </div>
+            <div>
+                <button onClick={() => deleteUserHandler(user.bindId)} disabled={isLoadingUsers}>Удалить жильца</button>
             </div>
         </div>
     );
