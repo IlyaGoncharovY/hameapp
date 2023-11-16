e49f3cd938e410330a76e44b1e6c79d122e5dd07
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-11-16 22:01:44 +0300

    fix(StocksContainer) added logic for render error and loaded, sequential request for added user, added inputs, added style

--- src/features/stocks/components/StocksContainer.tsx
+++ src/features/stocks/components/StocksContainer.tsx
@@ -1,8 +1,12 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 
 import {homeAPI} from "../../../api/HomeAppService";
+import {
+    addUserRequestType,
+    HousesResponseTypeChild,
+    StreetsResponseTypeChild
+} from "../../../common/interfaces/Interfaces";
 import {StockUserItem} from "../../users/components/StockUserItem";
-import {HousesResponseTypeChild, StreetsResponseTypeChild} from "../../../common/interfaces/Interfaces";
 
 interface IStocksContainer {
     house: HousesResponseTypeChild
@@ -17,9 +21,93 @@ export const StocksContainer: FC<IStocksContainer> = ({street, house}) => {
         isLoading
     } = homeAPI.useGetStocksQuery({streetsId: street.id, housesId: house.id})
 
+    const [addUser] = homeAPI.useAddNewUserMutation()
+
+    const [bindUserInApartment,
+        {
+            error: addUserError,
+            isLoading: addUserIsLoading
+        }
+    ] = homeAPI.useBindUserInApartmentMutation()
+
+    const [newUser, setNewUser] = useState<addUserRequestType>({
+        name: '',
+        phone: '',
+        email: '',
+    })
+
+    const handleAddUser = async () => {
+        try {
+            const response = await addUser(newUser)
+
+            if ('data' in response && response.data?.result === "Ok") {
+                const clientId = response.data.id;
+
+                const stockAddressId = stocks && stocks[0]?.addressId
+
+                if (stockAddressId) {
+                    await bindUserInApartment({
+                        addressId: stockAddressId,
+                        clientId: clientId,
+                    })
+                }
+            }
+
+            setNewUser({
+                name: '',
+                phone: '',
+                email: '',
+            })
+        } catch (error) {
+            console.error("Error adding new user:", error)
+        }
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewUser({
+            ...newUser,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    if (isLoading) {
+        return <h1>...Загрузка квартиры</h1>
+    }
+
+    if (error) {
+        return <h1>Произошла ошибка</h1>
+    }
+
     return (
-        <div style={{border: "2px solid gold"}}>
-            <div>StocksContainer</div>
+        <div style={{padding: "20px", margin: "20px"}}>
+            <div>Квартира</div>
+            <div>
+                <div>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Имя"
+                        value={newUser.name}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        type="text"
+                        name="phone"
+                        placeholder="Телефон"
+                        value={newUser.phone}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        value={newUser.email}
+                        onChange={handleInputChange}
+                    />
+                    {addUserError && <div>Что то не так с запросом =(</div>}
+                </div>
+                <button onClick={handleAddUser} disabled={addUserIsLoading}>Добавить жильца</button>
+            </div>
             <div>
                 <div>Номер дома: {house.id}</div>
                 <div>Имя дома: {house.name}</div>
@@ -29,7 +117,8 @@ export const StocksContainer: FC<IStocksContainer> = ({street, house}) => {
                     <StockUserItem
                         key={stock.addressId}
                         stock={stock}
-                    />)}
+                    />
+                )}
             </div>
         </div>
     );
